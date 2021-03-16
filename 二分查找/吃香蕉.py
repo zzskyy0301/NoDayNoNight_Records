ad30c2cbd71f0ac37d868e593343befa51a1b700
zzskyy0301 <77591939+zzskyy0301@users.noreply.github.com>
2021-03-16 19:53:15 -0400

    Create 吃香蕉.py

---
+++ 二分查找/吃香蕉.py
@@ -0,0 +1,21 @@
+#第875题目
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        l=1
+        r=max(piles)
+        
+        while l<r:
+            t=0
+            m=l+(r-l)//2
+            for p in piles:
+                t+=(p+m-1)/m
+            if t<=h:
+                r=m
+            else:
+                l=m+1
+        return l

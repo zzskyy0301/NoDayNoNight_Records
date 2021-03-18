604f305881ff83b00279e95b101d09e6ee586f77
zzskyy0301 <77591939+zzskyy0301@users.noreply.github.com>
2021-03-18 17:10:12 -0400

    easy 递归思想

---
+++ 链表/21.合并链表并排序.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1==None: return l2
+        if l2==None: return l1
+        if l1.val<l2.val:
+            l1.next=self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next=self.mergeTwoLists(l2.next,l1)
+            return l2

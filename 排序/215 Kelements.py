6a9d6e7be1f1e8aabef18358074e062e7d54321d
zzskyy0301 <77591939+zzskyy0301@users.noreply.github.com>
2021-03-21 15:20:06 -0400

    medium

--- 排序/215 Kelements.py
+++ 排序/215 Kelements.py
@@ -33,3 +33,44 @@ class Solution(object):
 链接：https://leetcode-cn.com/problems/kth-largest-element-in-an-array/solution/pythonkuai-pai-by-geng-chen-i-uo9c/
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''堆排序算法'''
+def heapify(arr,i):
+    if i >=len(arr)-1:
+        return arr
+    son1=2*i+1
+    son2=2*i+2
+    maxin=i
+    if son1<=(len(arr)-1) and arr[son1]>arr[maxin]:
+        maxin=son1
+    if son2<=(len(arr)-1) and arr[son2]>arr[maxin]:
+        maxin=son2
+    if maxin!=i:
+        temp=arr[i]
+        arr[i]=arr[maxin]
+        arr[maxin]=temp
+        heapify(arr,maxin)
+    return arr
+def build_heap(arr):
+    lastparent_index=(len(arr)-2)//2
+    #print(lastparent_index)
+    for i in range(lastparent_index,-1,-1):
+        #print(i)
+        heapify(arr,i)
+        #print(arr)
+    return arr
+
+arr=build_heap(arr)
+print(arr)
+for i in range(len(arr)-1,-1,-1):
+
+    temp=arr[0]
+    arr[0]=arr[i]
+    arr[i]=temp
+    if i==len(arr)-k:
+        return arr[i]
+    else:
+        arr[0:i]=heapify(arr[0:i],0)[:]
+
+
+return nums[[len(arr)-k]]
+

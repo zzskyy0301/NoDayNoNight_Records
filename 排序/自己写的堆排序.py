457bfe8e625f0771c7a8282d7d814ac64535c5d8
zzskyy0301 <77591939+zzskyy0301@users.noreply.github.com>
2021-03-21 15:08:03 -0400

    Create 自己写的堆排序.py

---
+++ 排序/自己写的堆排序.py
@@ -0,0 +1,37 @@
+def heapify(arr,i):
+      if i >=len(arr)-1:
+          return arr
+      son1=2*i+1
+      son2=2*i+2
+      maxin=i
+      if son1<=(len(arr)-1) and arr[son1]>arr[maxin]:
+          maxin=son1
+      if son2<=(len(arr)-1) and arr[son2]>arr[maxin]:
+          maxin=son2
+      if maxin!=i:
+        temp=arr[i]
+        arr[i]=arr[maxin]
+        arr[maxin]=temp
+        heapify(arr,maxin)
+      return arr
+def build_heap(arr):
+  lastparent_index=(len(arr)-2)//2
+  #print(lastparent_index)
+  for i in range(lastparent_index,-1,-1):
+    #print(i)
+    heapify(arr,i)
+    #print(arr)
+  return arr
+def heap_sort(arr):
+   arr=build_heap(arr)
+   print(arr)
+   for i in range(len(arr)-1,-1,-1):
+     print(arr[i])
+     temp=arr[0]
+     arr[0]=arr[i]
+     arr[i]=temp
+     print(arr[0:i])
+     arr[0:i]=heapify(arr[0:i],0).copy()
+     print('heapresult',arr)
+   return arr
+heap_sort([2,5,3,1,4,10])

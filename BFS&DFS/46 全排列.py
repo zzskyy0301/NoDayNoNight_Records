c17eb2a810ea09b2d4e4654ba7bc48a26481f7f4
zzskyy0301 <77591939+zzskyy0301@users.noreply.github.com>
2021-05-06 23:15:54 -0400

    medium

--- BFS&DFS/46 全排列.py
+++ BFS&DFS/46 全排列.py
@@ -9,7 +9,7 @@ class Solution(object):
         
         def backtrack(nums):
             if len(track)==len(nums):
-                result.append(track[:])
+                result.append(track[:])#这里一定要做一次拷贝，不然传递的是位置，最后返回空列表
                
                # return 
             for i in range(0,len(nums)):
@@ -19,5 +19,24 @@ class Solution(object):
                     track.pop()
                     #print(result)
             #return result
+        backtrack(nums)
+        return result
+
+    
+    
+            def backtrack(nums):
+            if len(track)==len(nums):
+                result.append(track[:])
+               
+                return
+            for i in range(len(nums)):        
+                if nums[i] not in track:
+                    track.append(nums[i]) 
+                                     
+                    backtrack(nums)
+                    track.pop()
+        track=[]
+        visited=[0 for i in range(len(nums))]
+        result=[]
         backtrack(nums)
         return result
